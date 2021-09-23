84daea36fd24f9b27daebfd193cff828fe88302c
BenAji <ayoadebenjamin@gmail.com>
2021-09-24 00:59:52 +0100

    Removed excess spaces in the code

--- Explore_bikeshare_data.r
+++ Explore_bikeshare_data.r
@@ -53,6 +53,7 @@ ggplot(aes(x=Birth.Year), data=subset(ny,!is.na(Gender))) +
   #scale_x_continuous(limits=c(1945,2000) +
   labs(x = "Year of Birth")
 
+#display the top rows of the washinghton dataset
 head(wash)
 
 #plot the graph showing Type of the users in Washinghton dataset
@@ -64,6 +65,7 @@ ggplot(aes(x=User.Type), data=wash) +
 #get the exact numbers of type-of-Users doing bikerent in Washinghton
 table(wash$User.Type)
 
+#summary statistics of washingthon dataset
 by(wash$Trip.Duration, wash$User.Type, summary)
 
 #this plots intends to show the station that clients usually starts their trip from
@@ -73,8 +75,7 @@ ggplot(aes(x=Start.Station), data=wash) +
   scale_y_discrete(limits=c(500,2000))+ #limit the y-axis to stations that has counts up to 500 and above
   labs(x = "Start Stations")
 
-# Your solution code goes here
-
+#display the top rows in the chicago dataset
 head(chi)
 
 #summary statistics of Chicago dataset
@@ -82,10 +83,8 @@ by(chi$Trip.Duration, chi$Gender, summary)
 
 #plot the graph showing Tripduration in Chicago dataset
 ggplot(aes(y=Trip.Duration, x=Gender), data=subset(chi,!is.na(Gender))) +
-  geom_boxplot(outlier.colour="black", outlier.shape=16,
-             outlier.size=2, notch=FALSE) + 
-  ggtitle('Chicago Bike Rental:Gender Trip Duration') + 
-      coord_cartesian(ylim = c(0,2000))+
+  geom_boxplot(outlier.colour="black", outlier.shape=16, outlier.size=2, notch=FALSE) + 
+  ggtitle('Chicago Bike Rental:Gender Trip Duration') + coord_cartesian(ylim = c(0,2000))+
   labs(x = "Gender")
 
 #get the exact number of users type doing bikerent in Chicago
